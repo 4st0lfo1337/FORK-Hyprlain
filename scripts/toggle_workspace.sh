015d859853834cba45e2376f713db44cac9f4d03
4st0lfo1337 <christianinamoto@proton.me>
2026-09-03 17:37:23 -0300

    Delete scripts directory

--- scripts/toggle_workspace.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-current=$(hyprctl activeworkspace -j | jq '.id')
-
-[[ "$current" == "9" ]] && hyprctl dispatch workspace 10 || hyprctl dispatch workspace 9
--- scripts/toggle_workspace2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-current=$(hyprctl activeworkspace -j | jq '.id')
-
-[[ "$current" == "7" ]] && hyprctl dispatch workspace 8 || hyprctl dispatch workspace 7
